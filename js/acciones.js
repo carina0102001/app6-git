d661f5bdc085e000e52e5fb98e9ad634a398bb93
Carina <mncarina.cm111@gmail.com>
2019-05-09 14:27:17 -0500

    5676748

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,7 @@
 $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){
 		
-		$('#izquierda').on("swipeleft",function(){
+		$('#Izquierda').on("swipeleft",function(){
 			navigator .notification. alert("Deslizo a la izquierda",function(){"Aplicacion","Aceptar"});
 });
 
